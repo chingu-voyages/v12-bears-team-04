996a853048a3a4dfcc3b040b0898aa478d8ce60e
guigui64 <guillaume.comte10@gmail.com>
2019-10-03 20:30:39 +0200

    wip dashboard

--- betcha-frontend/src/App.js
+++ betcha-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Route exact path="/" component={LandingPage} />
         <Route path="/SignUp" component={SignUp} />
+        <Route path="/Dashboard" component={Dashboard} />
       </div>
     </Router>
   );
