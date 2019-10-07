7419208a5591de4a51031e41db1e9dd167a230ca
Kieff612 <kieffwong@hotmail.com>
2019-10-07 23:42:23 +0100

    email auth

--- betcha-backend/functions/index.js
+++ betcha-backend/functions/index.js
@@ -84,32 +84,71 @@ db
  });
 });
 
-//signuproute
+const isEmail = (email) => {
+    // eslint-disable-next-line no-useless-escape
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+
+//sign up route
+//verify credentials
+// eslint-disable-next-line consistent-return
 app.post('/signup', (req, res) => {
     const newUser = {
         email: req.body.email,
         password: req.body.password,
         confirmPassword: req.body.confirmPassword,
-        handle: req.body.handle,
+        userName: req.body.userName,
     };
+
+    let errors = {};
+
+    if(isEmpty(newUser.email)) {
+        errors.email = 'Email must not be empty'
+    } else if(!isEmail(newUser.email)){
+        errors.email = 'Enter a valid email address'
+    }
+    if(isEmpty(newUser.password)) errors.password = 'Please enter your Password'
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Password does not match, Please make sure enter exact same password again';
+    if(isEmpty(newUser.userName)) errors.userName = 'Pleasenter your username'
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
     //TODO: validate date
-    db.doc(`/users/${newUser.handle}`).get()
-    .then(doc => {
+    let token, userId;
+    db.doc(`/users/${newUser.userName}`)
+    .get()
+    .then((doc) => {
         if(doc.exists){
-            return res.status(400).json({ handle: 'this handle is already taken'});
+            return res.status(400).json({ userName: 'The userName is already taken'});
         } else {
             return firebase
             .auth()
             .createUserWithEmailAndPassword(newUser.email, newUser.password)
         }
     })
-    .then(data => {
-       return data.user.getIdToken()
+    .then((data) => {
+        userId = data.user.uid;
+       return data.user.getIdToken();
     })
-    .then(token => {
-        return res.status(201).json({ token })
+    .then((idToken) => {
+        token = idToken;
+        const userCredentials = {
+            userName: newUser.userName,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId
+        };
+        return db.doc(`/users/${newUser.userName}`).set(userCredentials);
     })
-    .catch(err => {
+    .then(() => {
+        return res.status(201).json({ token });
+    })
+    .catch((err_) => {
         console.error(err);
         if(err.code ===  "auth/email-already-in-use") {
             return res.status(400).json({ email:  "Email is already in use"})
@@ -118,4 +157,30 @@ app.post('/signup', (req, res) => {
     }
 });
 });
+
+// eslint-disable-next-line consistent-return
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+    if(isEmpty(user.email)) errors.email = 'Enter a Valid email';
+    if(isEmpty(user.password)) errors.password = 'Enter a valid password';
+
+    if(Object.kets(errors).length > 0) return res.status(400).json(errors);
+    firebase.auth().signInWithEmailAndPassword(user.email, user.passord)
+    .then((data) => {
+        return data.getIdToken();
+    })
+    .then((token) => {
+        return res.json({ token });
+    }) 
+    .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code })
+    })
+})
+
 exports.api = functions.region("europe-west1").https.onRequest(app);
